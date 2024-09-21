706bdec8950eec1f814ccfcae46a1733f478d935
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2024-09-21 16:38:17 +0700

    Added JUnit

--- src/test/BeanTest.java
+++ src/test/BeanTest.java
@@ -1,5 +1,14 @@
 package test;
 
-public class BeanTest {
-	
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BeanTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
 }
